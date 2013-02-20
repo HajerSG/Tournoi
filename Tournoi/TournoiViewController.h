587286792c72694d9922605486dd00ba9a39d796
HajerSG <hajer.bazaa@ingesup.com>
2013-02-20 12:06:27 +0100

    first commit

--- Tournoi/TournoiViewController.h
+++ Tournoi/TournoiViewController.h
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface TournoiViewController : UIViewController
+@interface TournoiViewController : UIViewController<UITableViewDelegate, UITableViewDataSource>
+
 
 @end
--- Tournoi/TournoiViewController.m
+++ Tournoi/TournoiViewController.m
@@ -13,17 +13,40 @@
 @end
 
 @implementation TournoiViewController
+{
+    NSArray *tableData;
+}
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+   [super viewDidLoad];
+	// Initialize table data
+    tableData = [NSArray arrayWithObjects:@"La difference entre toi et moi c'est que moi j'ai de la classe", @"Même apres la 3ème démarque j'ai pas besoin de toi", @"t'es aussi insupportable qu'un Kiko lol", @"Pour toi j'irais décrocher la lune", @"Veux tu être ma valentine???", nil];
 }
-
-- (void)didReceiveMemoryWarning
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    return [tableData count];
 }
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"SimpleTableItem";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+    
+    cell.textLabel.text = [tableData objectAtIndex:indexPath.row];
+    return cell;
+}
+//- (void)didReceiveMemoryWarning
+//{
+  //  [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+//}
+
+   
+
 
 @end
